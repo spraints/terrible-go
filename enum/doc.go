b81a46ca462e6cc0b2c40422f470d53828ff34e4
Matt Burke <spraints@gmail.com>
2019-07-12 10:55:39 -0400

    in the beginning there was a very permissive type alias

---
+++ enum/doc.go
@@ -0,0 +1,9 @@
+// Package enum answers the question, what if Go had more functional primitives?
+//
+// None of this is particularly safe, and will panic in case there are any errors.
+package enum
+
+// Enum is a wrapper around any type.
+//
+// Usage: enum.Enum(whatevs)
+type Enum interface{}
